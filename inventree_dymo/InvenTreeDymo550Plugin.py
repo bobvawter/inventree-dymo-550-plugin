9751565b8f966f4d30a9e597c6fec369c0500936
Bob Vawter <bob@vawter.org>
2025-07-04 18:11:52 -0400

    Begin to productionize code
    Notable changes:
    * Separate out a Conn class to handle the network messages, greatly simplifying
      the driver plugin class.
    * The Conn class will dial on demand since the only indication of an
      already-busy printer is a TCP RST.
    * Introduce a Status class to decode the ESC A command results.
    * Add a test rig for Conn and Status.

--- inventree_dymo/InvenTreeDymo550Plugin.py
+++ inventree_dymo/InvenTreeDymo550Plugin.py
@@ -1,7 +1,5 @@
 import logging
-import math
 import socket
-import time
 
 from django.db import models
 from django.db.models.query import QuerySet
@@ -11,6 +9,8 @@ from plugin.machine.machine_types import LabelPrinterBaseDriver, LabelPrinterMac
 from report.models import LabelTemplate
 from rest_framework import serializers
 
+from .conn import Conn, LockIntent, Speed
+from .status import PrinterState, StatusReport, StatusReportError
 from .version import DYMO_PLUGIN_VERSION
 
 logger = logging.getLogger('inventree')
@@ -28,10 +28,6 @@ class InvenTreeDymo550Plugin(InvenTreePlugin):
     VERSION = DYMO_PLUGIN_VERSION
 
 
-SPEED_GRAPHICS = 'graphics'
-SPEED_TEXT = 'text'
-SPEED_TURBO = 'turbo'
-
 class Dymo550LabelPrinterDriver(LabelPrinterBaseDriver):
     """Label printer driver for Dymo 550 printers."""
 
@@ -41,8 +37,8 @@ class Dymo550LabelPrinterDriver(LabelPrinterBaseDriver):
 
     class PrintingOptionsSerializer(LabelPrinterBaseDriver.PrintingOptionsSerializer):
         speed = serializers.ChoiceField(
-            choices=[(SPEED_GRAPHICS, _('Graphics')), (SPEED_TEXT, _('Text')), (SPEED_TURBO, _("Turbo"))],
-            default=SPEED_GRAPHICS,
+            choices=[(Speed.GRAPHICS, _('Graphics')), (Speed.TEXT, _('Text')), (Speed.TURBO, _("Turbo"))],
+            default=Speed.GRAPHICS,
             label=_('Print Speed'),
             help_text=_('Trade print quality for speed')
         )
@@ -67,131 +63,69 @@ class Dymo550LabelPrinterDriver(LabelPrinterBaseDriver):
 
         super().__init__(*args, **kwargs)
 
-    def get_printing_options_serializer(self, request, *args, **kwargs):
+    def get_printing_options_serializer(self, _, *args, **kwargs):
         return self.PrintingOptionsSerializer(*args, **kwargs)
+
+    def init_machine(self, machine: LabelPrinterMachine):
+        self.restart_machine(machine)
+
+    def restart_machine(self, machine: LabelPrinterMachine):
+        try:
+            with Conn(machine.get_setting('SERVER', 'D'), machine.get_setting('PORT', 'D')) as c:
+                self.set_status_from_report(machine, c.status_report())
+        except Exception as e:
+            machine.set_status(LabelPrinterMachine.MACHINE_STATUS.DISCONNECTED)
+            machine.handle_error(e)
+
+    def update_machine(self, _, machine: LabelPrinterMachine):
+        self.restart_machine(machine)
 
     def print_labels(self, machine: LabelPrinterMachine, label: LabelTemplate, items: QuerySet[models.Model], **kwargs):
         """Print labels using a Dymo label printer."""
         printing_options = kwargs.get('printing_options', {})
-        logger.debug("print_labels running")
+        speed = Speed(printing_options.get('speed', Speed.GRAPHICS))
+        ip = machine.get_setting('SERVER', 'D')
+        port = machine.get_setting('PORT', 'D')
 
-        self.open_socket(machine.get_setting('SERVER', 'D'), machine.get_setting('PORT', 'D'))
         try:
-            self.wait_for_unlocked()
-            self.wait_for_lock()
-            self.start_job(speed=printing_options.get('speed', SPEED_GRAPHICS))
+            with Conn(ip, port) as c:
+                # Acquire the lock on the printer. This will block if another
+                # print job is underway.
+                report = c.wait_until_state(PrinterState.IDLE, intent=LockIntent.LOCK)
 
-            index = 1
-            for item in items:
-                png = self.render_to_png(label, item, dpi=300).rotate(90, expand=1)
+                if not report.can_print:
+                    raise StatusReportError("printer requires attention", report)
 
-                for _copies in range(printing_options.get('copies', 1)):
-                    self.send_label(index, png)
-                    index = index + 1
+                machine.set_status(LabelPrinterMachine.MACHINE_STATUS.PRINTING)
 
-            # Advance to tear-off position
-            self.send_command("E")
+                # Send start commands to the printer.
+                c.start_job(speed)
 
-            # End job.
-            self.send_command("Q")
+                # Spool each image to the printer.
+                index = 1
+                for item in items:
+                    png = self.render_to_png(label, item, dpi=Conn.DPI)
+
+                    for _copies in range(printing_options.get('copies', 1)):
+                        c.send_label(index, png)
+                        index = index + 1
+
+                # Advance to tear-off position.
+                c.send_command("E")
+
+                # End job.
+                c.send_command("Q")
+                machine.set_status(LabelPrinterMachine.MACHINE_STATUS.CONNECTED)
+        except StatusReportError as e:
+            self.set_status_from_report(machine, e.status)
+            machine.handle_error(e)
 
         except Exception as e:
-            logger.error(e, exc_info=True)
-            raise e
+            machine.set_status(LabelPrinterMachine.MACHINE_STATUS.DISCONNECTED)
+            machine.handle_error(e)
 
-        finally:
-            if self.print_socket is not None:
-                self.print_socket.close()
-
-    def open_socket(self, ip_addr: str, port: int):
-        self.print_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.print_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-        self.print_socket.connect((ip_addr, port))
-        logger.debug("socket connected")
-
-    def wait_for_idle(self):
-        while True:
-            self.send_command("A", 0)  # Non-locking status request
-            status = self.print_socket.recv(32)
-            logger.debug("status: %s", status.hex())
-            if status[0] == 0:
-                return
-            logger.debug("waiting for idle")
-            time.sleep(0.5)
-
-    def wait_for_lock(self):
-        while True:
-            self.send_command("A", 1)  # Locking status request
-            status = self.print_socket.recv(32)
-            logger.debug("status: %s", status.hex())
-            if status[0] == 0:
-                err_code = (status[26] << 24) | (status[25] << 16) | (status[24] << 8) | (status[23])
-                if err_code != 0:
-                    raise Exception(f"printer reports error code {err_code}")
-                # Have the lock, make sure the media is ready.
-                if status[10] == 6 or status[10] == 7 or status[10] == 8:
-                    return
-                raise Exception(f"unexpected media state {status[10]}")
-            logger.debug("waiting to lock")
-            time.sleep(0.5)
-
-    def wait_for_unlocked(self):
-        while True:
-            self.send_command("A", 0)  # Non-locking status request
-            status = self.print_socket.recv(32)
-            logger.debug("status: %s", status.hex())
-            if status[0] == 5:
-                return
-            logger.debug("waiting for unlocked state")
-            time.sleep(0.5)
-
-    def send_command(self, cmd: str, *more: int):
-        """ A utility method to send an escape command """
-        out = bytearray([0x1b, ord(cmd)]) + bytearray(more)
-        logger.debug(out.hex())
-        self.print_socket.sendall(out)
-
-    def start_job(self, speed: str = SPEED_GRAPHICS):
-        self.send_command("s", 1, 0, 0, 0)  # start job 1 (little-endian order)
-        self.send_command("e")  # use default density
-        self.send_command("i" if speed == SPEED_GRAPHICS else "h")  # Graphics or text mode
-        self.send_command("T", 0x20 if speed == SPEED_TURBO else 0x10)  # High- or normal-speed
-        self.send_command("L", 0, 0)  # use chip-based media length
-
-    def send_label(self, index, png):
-        width, height = png.size
-        bytes_per_line = math.ceil(width / 8)
-        dot_height = bytes_per_line * 8
-
-        # Convert to B&W, then rotate into column-major order.
-        data = png.convert('L').point(lambda x: 0 if x > 200 else 1, mode='1').tobytes()
-        data = [data[y * bytes_per_line:(y + 1) * bytes_per_line] for y in range(height)]
-        data = bytearray(b''.join(data))
-
-        # We've swapped the dimensions.
-        height, width = png.size
-        logger.debug("data is %d bytes long width=%d height=%d, bytes_per_line=%d, dot_height=%d",
-                     len(data), width, height, bytes_per_line, dot_height)
-
-        # Start of label.
-        self.send_command("n", index & 0xFF, (index >> 8) & 0xFF)
-
-        # Send label data header.
-        self.send_command(
-            "D",
-            1,  # bits per pixel, always 1
-            2,  # align to bottom of label, always 2
-            width & 0xFF,
-            (width >> 8) & 0xFF,
-            (width >> 16) & 0xFF,
-            (width >> 24) & 0xFF,
-            dot_height & 0xFF,
-            (dot_height >> 8) & 0xFF,
-            (dot_height >> 16) & 0xFF,
-            (dot_height >> 24) & 0xFF
-        )
-
-        self.print_socket.sendall(data)
-
-        # Feed to start of next label.
-        self.send_command("G")
+    def set_status_from_report(self, machine: LabelPrinterMachine, report: StatusReport):
+        if not report.can_print:
+            machine.set_status(LabelPrinterMachine.MACHINE_STATUS.NO_MEDIA)
+        else:
+            machine.set_status(LabelPrinterMachine.MACHINE_STATUS.CONNECTED)
